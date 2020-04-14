4e5efd49a5e3c9382379fba5203c996ebe9c0f88
Rafael Mellado <rafael.mellado@pucv.cl>
2020-04-14 18:12:14 -0400

    Add files via upload

---
+++ CovidCL/src/covidcl/CovidCL.java
@@ -0,0 +1,77 @@
+package covidcl;
+import java.io.*;
+
+/**
+ *
+ * @author rafaelmellado
+ */
+public class CovidCL 
+{
+    
+    public static void main(String[] args) throws IOException
+    {
+        int cadenaADN=0, opción;
+        double factorComparación=0;
+        
+        BufferedReader lector = new BufferedReader(new InputStreamReader (System.in));
+        
+        do
+        {
+            System.out.println("Menú de usuario: ");
+            System.out.println("1. Ingresar cadena de ADN");
+            System.out.println("2. Ingresar factor de comparación");
+            System.out.println("3. Verificar mutación");
+            System.out.println("Otro: Salir");
+            System.out.println("\n Ingrese opción: ");
+            opción=Integer.parseInt(lector.readLine());
+            
+            
+            switch (opción)
+            {
+                case 1:
+                    do
+                    {
+                        System.out.println("Ingrese valor de cadena de ADN, recuerde que debe ser positivo: ");
+                        cadenaADN=Integer.parseInt(lector.readLine());
+                    }while(cadenaADN<=0);
+                    break;
+                case 2:
+                    System.out.println("Ingrese valor de factor de comparación: ");
+                    factorComparación=Double.parseDouble(lector.readLine());
+                    break;
+                case 3:
+                    /*Primero se invierte cadena ADN*/
+                    int ADNInvertido=0;
+           
+                    while(cadenaADN>0)
+                    {
+                        int digito=cadenaADN%10;
+                        cadenaADN=cadenaADN/10;
+                        ADNInvertido=(ADNInvertido*10)+digito;
+                    }
+                    
+                    /*Se calcula raiz*/
+                    boolean calculado=false;
+                    for(double raiz=0.1; ADNInvertido>=raiz*raiz && calculado==false; raiz=raiz+0.1)
+                    {
+                        if(raiz*raiz>=ADNInvertido)
+                        {
+                            if(Math.abs(raiz-factorComparación)<0.0000001)
+                            {
+                                System.out.println("El virus mutó a buena persona");
+                            }
+                            else
+                            {
+                                System.out.println("El virus no ha mutado");
+                            }
+                            calculado=true;
+                        }
+                    }
+                    break;
+                default:
+                    System.out.println("Adios!");
+                   
+            } 
+        }while(opción!=5);
+    }
+}
